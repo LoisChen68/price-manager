404f31260260bbde9554afa785d4bcafd0a8983d
loischen68 <a8283626@gmail.com>
2024-04-15 01:39:21 +0800

    feat: getNumberIntervals function and AgeGroupSelect isOverlay

--- src/components/AgeGroupSelect.jsx
+++ src/components/AgeGroupSelect.jsx
@@ -12,6 +12,9 @@ const { Text } = Typography;
 
 const StyledSelect = styled(Select)`
   width: 40%;
+  &.ant-select > .ant-select-selector {
+    ${(props) => (props.verify === "error" ? "border-color: red;" : "")};
+  }
 `;
 
 const StyledInput = styled(Input)`
@@ -22,68 +25,139 @@ const StyledInput = styled(Input)`
   text-align: center;
 `;
 
-export default function AgeGroupSelect({ id, setAgeGroupPrice }) {
+const StyledHightLightText = styled(Text)`
+  background-color: ${(props) => (props.type === "danger" ? "#FED7D785" : "")};
+  border-radius: 5px;
+  padding: 2px 5px 2px 5px;
+  width: 95%;
+`;
+
+function splitNumberIntervals(arr) {
+  const numberIntervals = [];
+  let start = arr[0];
+  let end = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === end + 1) {
+      end = arr[i];
+    } else {
+      numberIntervals.push([start, end]);
+      start = arr[i];
+      end = arr[i];
+    }
+  }
+  numberIntervals.push([start, end]);
+  return numberIntervals;
+}
+
+function getNumberIntervals(arr) {
+  const numberRangerArr = [];
+  const numberSet = new Set();
+  const duplicateSet = new Set();
+
+  arr.map((arr) => {
+    const start = arr[0];
+    const end = arr[1] + 1;
+    for (let i = start; i < end; i++) {
+      numberRangerArr.push(i);
+    }
+    return false;
+  });
+  numberRangerArr.forEach((value) => {
+    numberSet.has(value) ? duplicateSet.add(value) : numberSet.add(value);
+  });
+  const maxNumber = Math.max(...numberSet);
+  const numberArr = [];
+  for (let i = 0; i < maxNumber + 1; i++) {
+    numberArr.push(i);
+  }
+
+  const numbers = Array.from(numberSet);
+  const duplicates = Array.from(duplicateSet);
+  const filterNumberRepeat = numberArr.filter((v) => !numbers.includes(v));
+  const overlay = splitNumberIntervals(duplicates);
+  const noIncludes = splitNumberIntervals(filterNumberRepeat);
+  return { overlay, noIncludes };
+}
+
+export default function AgeGroupSelect({
+  id,
+  setAgeGroupPrice,
+  ageGroupPrice,
+}) {
   const [startAge, setStartAge] = useState(0);
   const [endAge, setEndAge] = useState(0);
   const [startAgeOptions, setStartAgeOptions] = useState(ageOptions);
   const [endAgeOptions, setEndAgeOptions] = useState(ageOptions);
+  const overlay = getNumberIntervals(
+    ageGroupPrice.map((v) => v.ageGroup)
+  ).overlay;
+  const isOverlay = overlay.flat().some((v) => v !== undefined);
 
   return (
     <Flex vertical>
-      <Text type="secondary">年齡</Text>
-      <Space.Compact block>
-        <StyledSelect
-          defaultValue="0"
-          options={startAgeOptions}
-          onChange={(value) => {
-            const options = endAgeOptions.map((option) => {
-              if (option.value < value) {
-                return { ...option, disabled: true };
-              } else {
-                return { ...option, disabled: false };
-              }
-            });
-            setEndAgeOptions(options);
-            setStartAge(value);
-            setAgeGroupPrice((pre) => {
-              const exist = pre.some((p) => p.id === id);
-              if (exist) {
-                return pre.map((p) =>
-                  p.id === id ? { ...p, ageGroup: [value, endAge] } : p
-                );
-              } else {
-                return [...pre, { id, ageGroup: [value, endAge] }];
-              }
-            });
-          }}
-        />
-        <StyledInput className="site-input-split" placeholder="～" disabled />
-        <StyledSelect
-          defaultValue="0"
-          options={endAgeOptions}
-          onChange={(value) => {
-            const options = startAgeOptions.map((option) => {
-              if (option.value > value) {
-                return { ...option, disabled: true };
-              } else {
-                return { ...option, disabled: false };
-              }
-            });
-            setStartAgeOptions(options);
-            setEndAge(value);
-            setAgeGroupPrice((pre) => {
-              const exist = pre.some((p) => p.id === id);
-              if (exist) {
-                return pre.map((p) =>
-                  p.id === id ? { ...p, ageGroup: [startAge, value] } : p
-                );
-              } else {
-                return [...pre, { id, ageGroup: [startAge, value] }];
-              }
-            });
-          }}
-        />
-      </Space.Compact>
+      <Flex vertical>
+        <Text type="secondary">年齡</Text>
+        <Space.Compact block>
+          <StyledSelect
+            verify={!!isOverlay ? "error" : ""}
+            defaultValue="0"
+            options={startAgeOptions}
+            onChange={(value) => {
+              const options = endAgeOptions.map((option) => {
+                if (option.value < value) {
+                  return { ...option, disabled: true };
+                } else {
+                  return { ...option, disabled: false };
+                }
+              });
+              setEndAgeOptions(options);
+              setStartAge(value);
+              setAgeGroupPrice((pre) => {
+                const exist = pre.some((p) => p.id === id);
+                if (exist) {
+                  return pre.map((p) =>
+                    p.id === id ? { ...p, ageGroup: [value, endAge] } : p
+                  );
+                } else {
+                  return [...pre, { id, ageGroup: [value, endAge] }];
+                }
+              });
+            }}
+          />
+          <StyledInput className="site-input-split" placeholder="～" disabled />
+          <StyledSelect
+            verify={!!isOverlay ? "error" : ""}
+            defaultValue="0"
+            options={endAgeOptions}
+            onChange={(value) => {
+              const options = startAgeOptions.map((option) => {
+                if (option.value > value) {
+                  return { ...option, disabled: true };
+                } else {
+                  return { ...option, disabled: false };
+                }
+              });
+              setStartAgeOptions(options);
+              setEndAge(value);
+              setAgeGroupPrice((pre) => {
+                const exist = pre.some((p) => p.id === id);
+                if (exist) {
+                  return pre.map((p) =>
+                    p.id === id ? { ...p, ageGroup: [startAge, value] } : p
+                  );
+                } else {
+                  return [...pre, { id, ageGroup: [startAge, value] }];
+                }
+              });
+            }}
+          />
+        </Space.Compact>
+      </Flex>
+      {!!isOverlay && (
+        <StyledHightLightText type="danger">
+          年齡區間不可重疊
+        </StyledHightLightText>
+      )}
     </Flex>
   );
 }
